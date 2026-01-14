3736774e2790719760159e58e75039ff42e9cd1d
Edwin Tuinstra <123944378+Edwin9865@users.noreply.github.com>
2026-01-14 11:45:29 +0100

    Updated AndroidManifest.xml

--- android/app/src/main/java/com/coparenting/app/MainActivity.java
+++ android/app/src/main/java/com/coparenting/app/MainActivity.java
@@ -1,5 +1,60 @@
 package com.coparenting.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.util.Log;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Bridge;
+
+public class MainActivity extends BridgeActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setupWebViewCrashHandler();
+        }
+    }
+
+    private void setupWebViewCrashHandler() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WebView webView = getBridge().getWebView();
+            if (webView != null) {
+                webView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean onRenderProcessGone(WebView view, android.webkit.RenderProcessGoneDetail detail) {
+                        Log.e(TAG, "WebView render process gone. Did crash: " + detail.didCrash());
+
+                        if (detail.didCrash()) {
+                            Log.e(TAG, "WebView renderer crashed. Attempting recovery...");
+
+                            runOnUiThread(() -> {
+                                try {
+                                    if (view != null && view.getParent() != null) {
+                                        ((android.view.ViewGroup) view.getParent()).removeView(view);
+                                    }
+                                    view.destroy();
+
+                                    recreate();
+                                } catch (Exception e) {
+                                    Log.e(TAG, "Error during WebView recovery", e);
+                                    finish();
+                                    System.exit(0);
+                                }
+                            });
+
+                            return true;
+                        }
+
+                        return false;
+                    }
+                });
+            }
+        }
+    }
+}
