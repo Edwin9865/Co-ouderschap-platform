0d461a55ac40bda3741123139e51d538486ab54b
Edwin Tuinstra <123944378+Edwin9865@users.noreply.github.com>
2026-02-21 09:07:48 +0100

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -518,7 +518,7 @@ export function Dashboard() {
     <div className="space-y-6">
       <div>
         <h1 className="text-2xl sm:text-3xl font-bold text-gray-900">Dashboard</h1>
-        <p>BUILDTEST 2026-02-20 13:20</p>
+        <p>BUILDTEST 2026-02-21 09:10</p>
         <p className="mt-2 text-sm sm:text-base text-gray-600">Overzicht van recente activiteiten en openstaande items</p>
       </div>
 
