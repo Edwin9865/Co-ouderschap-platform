7a383d4fd61a830c17cada0f8058cc141d53461a
Edwin Tuinstra <123944378+Edwin9865@users.noreply.github.com>
2026-03-15 20:44:37 +0100

    1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.coparenting.app',
+  appId: 'nl.coouderschap.app',
   appName: 'CoParenting',
   webDir: 'dist',
   plugins: {
